bd3ea64d871a07855df0282bd334bd4e1ade43d6
Vermouth-93 <honeyantaka@yahoo.com>
2015-11-29 02:24:31 +0200

    Construct tables and basic methods
    Methods that convert decimal to hexadecimal numbers, add hexadecimal
    numbers and test operand field for hexadecimal addresses

---
+++ src/pass1_1/Pass1_1.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pass1_1;
+
+import java.util.Hashtable;
+
+/**
+ *
+ * @author Heba
+ */
+public class Pass1_1 {
+
+    /**
+     */
+    public static String locctr;
+
+    static String addHex(String inputHex) {
+        Integer inputDec = Integer.parseInt(inputHex, 16);
+        inputDec += 3;
+        String outputHex = Integer.toHexString(inputDec);
+        return outputHex;
+    }
+
+    static String decToHex(int deci) {
+        String hexa = Integer.toHexString(deci);
+        return hexa;
+    }
+
+    static boolean testHexOperand(String x) {
+        return x.startsWith("0");
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Hashtable optab = new Hashtable();
+        optab.put("add", 24);
+        optab.put("and", 64);
+        optab.put("comp", 40);
+        optab.put("div", 36);
+        optab.put("j", 60);
+        optab.put("jeq", 48);
+        optab.put("jgt", 52);
+        optab.put("jlt", 56);
+        optab.put("jsub", 72);
+        optab.put("lda", 0);
+        optab.put("ldch", 80);
+        optab.put("ldl", 8);
+        optab.put("ldx", 4);
+        optab.put("mul", 32);
+        optab.put("or", 68);
+        optab.put("rd", 216);
+        optab.put("rsub", 76);
+        optab.put("sta", 12);
+        optab.put("stch", 84);
+        optab.put("stl", 20);
+        optab.put("stx", 16);
+        optab.put("sub", 28);
+        optab.put("td", 224);
+        optab.put("tix", 44);
+        optab.put("wd", 220);
+        
+        Hashtable symtab = new Hashtable();
+
+    }
+
+}
