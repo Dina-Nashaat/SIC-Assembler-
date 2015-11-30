3b1ffcf27390b88513f94bc50dc465eb8ae42b95
Vermouth-93 <honeyantaka@yahoo.com>
2015-11-30 23:35:15 +0200

    Final edit of Pass 1
    Fixed minor bugs in handling comment lines and location counting

--- src/pass1_1/Pass1_1.java
+++ src/pass1_1/Pass1_1.java
@@ -14,6 +14,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import static java.lang.System.exit;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Hashtable;
@@ -30,7 +31,9 @@ public class Pass1_1 {
     /**
      */
     public static String locctr = "0";
-    
+    public static String startAddress = "0";
+    public static int programLength;
+
     public static ArrayList readAllLines() {
         ArrayList<String> lines = new ArrayList<String>();
         try {
@@ -45,36 +48,36 @@ public class Pass1_1 {
     }
 
     public static void writeLine(String line, File file) {
-            
+
         try {
             FileWriter fw = new FileWriter(file, true);
             PrintWriter writer = new PrintWriter(fw);
-            writer.println(line);
+            writer.println(line.toLowerCase());
             writer.close();
         } catch (IOException ex) {
             Logger.getLogger(Pass1_1.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    static String addHex(String inputHex, int i) {
+    public static String addHex(String inputHex, int i) {
         Integer inputDec = Integer.parseInt(inputHex, 16);
         inputDec += i;
         String outputHex = Integer.toHexString(inputDec);
         return outputHex;
     }
 
-    static String decToHex(int deci) {
+    public static String decToHex(int deci) {
         String hexa = Integer.toHexString(deci);
         return hexa;
     }
 
-    static boolean testHexOperand(String x) {
+    public static boolean testHexOperand(String x) {
         return x.startsWith("0");
     }
 
     public static String readStm(String stmt, String type) {
         type = type.toLowerCase();
-        int start = 0, end = 0;
+        int start, end;
         switch (type) {
             case "label":
                 start = 0;
@@ -89,31 +92,29 @@ public class Pass1_1 {
                 end = stmt.length();
                 break;
             default:
-                return "unidentified operation";
+                return "Unidentified operation";
         }
         String trgStm = stmt.substring(start, end).trim().toLowerCase();
         return trgStm;
     }
 
     public static boolean isComment(String stmt) {
-        if (stmt.startsWith(".")) {
-            return true;
-        }
-        return false;
+        return stmt.startsWith(".");
     }
 
     public static void main(String[] args) {
         // TODO code application logic here 
         File file = new File("INTFILE");
-        if(file.exists()) {
+        if (file.exists()) {
             file.delete();
-        }else
+        } else {
             try {
                 file.createNewFile();
-        } catch (IOException ex) {
-            Logger.getLogger(Pass1_1.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Pass1_1.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
-        String startAddress = "0";
+
         Hashtable optab = new Hashtable();
         optab.put("add", 24);
         optab.put("and", 64);
@@ -147,82 +148,79 @@ public class Pass1_1 {
         lines = readAllLines();
         String line;
         int i = 0;
-        line = lines.get(i);
+        line = lines.get(i);                                                                                 //Read next input line
         while (isComment(line)) {
             i++;
             line = lines.get(i);
         }
         if (readStm(line, "opcode").equals("start")) //if OPCODE = 'START' then
         {
-            startAddress = readStm(line, "operand");                            //save #[operand] as starting address
-            locctr = startAddress;                                              //initialize LOCCTR to starting address
-            //writeLine(line, file);                                                    //Write line to intermediate file
-            line = lines.get(i);                                                //Read next input line
+            startAddress = readStm(line, "operand");                                    //save #[operand] as starting address
+            locctr = startAddress;                                                                 //initialize LOCCTR to starting address
+            //writeLine(line, file);                                                                  //Write line to intermediate file
             //System.out.println(startAddress);
         } else {
-            locctr = "0";                                                       //else initialize locctr to zero
+            locctr = "0";                                                                                    //else initialize locctr to zero
         }
+        i++;
+        line = lines.get(i);
         while (!readStm(line, "opcode").equals("end")) //while OPCODE != END
         {
-            line = lines.get(i);                                                //Get line. 
-            if (!isComment(line)) {                                             //If line is not a comment
-                if (symtab.containsKey(readStm(line, "label"))) {               //If there is a label in the LABEL field, Search symtab for Label
-                    printError("duplicate error");                              //Print out Error
-                } else {                                                        //If not
-                    if(!(readStm(line, "label").equals("")))
-                        symtab.put(readStm(line, "label"), locctr);                 //Insert Label into symtable
+            if (!isComment(line)) {                                                                       //If line is not a comment
+                if (symtab.containsKey(readStm(line, "label"))) {                       //If there is a label in the LABEL field, Search symtab for Label
+                    printError("duplicate error");                                                 //Print out Error
+                } else{                                                                                           //If not
+                    if (!(readStm(line, "label").equals(""))) {
+                    symtab.put(readStm(line, "label"), locctr);                        //Insert Label into symtable
                 }
-                if (optab.containsKey(readStm(line, "opcode"))) {               //Search optab for opcode
-                    locctr = addHex(locctr, 3);                                  //Found; Add 3 to locctr
-                } else if (readStm(line, "opcode").equals("word")) {            //opcode not found but equal to WORD
-                    locctr = addHex(locctr, 3);                                  //Founnd; Add 3 to locctr
-                } else if (readStm(line, "opcode").equals("resw")) {            //opcode not found but equal to RESW
-                    try {
-                        int operandOp = (3 * Integer.parseInt(readStm(line, "operand"), 16));
-                        locctr = addHex(locctr, operandOp);                      //Found; 3*opcode+locctr
-                    } catch (Exception ex) {
-                        printError("Invalid Operator");
-                    }
-                } else if (readStm(line, "opcode").equals("resb")) {            //opcode not found but equal to BYTE
-                    try {
-                        locctr = addHex(locctr, Integer.parseInt(readStm(line, "opcode")));  //Found; add #operand to locctr
-                    } catch (Exception ex) {
-                        printError("Invalid Operator");
-                    }
+                }
 
-                } else if (readStm(line, "opcode").equals("byte")) {
-                    int byteLength = readStm(line, "opcode").length();           //find length in bytes
+                if (optab.containsKey(readStm(line, "opcode"))) {                   //Search optab for opcode
+                    locctr = addHex(locctr, 3);                                                  //Found; Add 3 to locctr
+                } else if (readStm(line, "opcode").equals("word")) {               //opcode not found but equal to WORD
+                    locctr = addHex(locctr, 3);                                                 //Founnd; Add 3 to locctr
+                } else if (readStm(line, "opcode").equals("resw")) {                //opcode not found but equal to RESW
                     try {
-                        locctr = addHex(locctr, byteLength);                     //add length to LOCCTR
+                        int operandOp = 3 * Integer.parseInt(readStm(line, "operand"));
+                        locctr = addHex(locctr, operandOp);                               //Found; 3*operand+locctr
                     } catch (Exception ex) {
                         printError("Invalid Operator");
                     }
-                } else if (readStm(line, "opcode").equals("start")) {
-                    writeLine(line,file);
-                    i++;
-                    continue;
-                } else if (readStm(line, "opcode").equals("end")) {
-                    writeLine(line,file);
-                    i++;
-                    break;
+                } else if (readStm(line, "opcode").equals("resb")) {                  //opcode not found but equal to BYTE
+                    try {
+                        locctr = addHex(locctr, Integer.parseInt(readStm(line, "operand")));  //Found; add #operand to locctr
+                    } catch (Exception ex) {
+                        printError("Invalid Operator");
+                    }
+                } else if (readStm(line, "opcode").equals("byte")) {
+                    int byteLength = readStm(line, "operand").length();                //find length in bytes
+                    try {
+                        locctr = addHex(locctr, byteLength);                                //add length to LOCCTR
+                    } catch (Exception ex) {
+                        printError("Invalid Operator");
+                    }
                 } else {
                     printError("Invalid Operation Code");
                 }
+                writeLine(line, file);
             }
-            writeLine(line,file);
             i++;
+            line = lines.get(i);
+        }
+        if (readStm(line, "opcode").equals("end")) {
+            writeLine(line, file);
         }
         try {
-            int programLength = Integer.parseInt(locctr) - Integer.parseInt(startAddress);
+            programLength = Integer.parseInt(locctr, 16) - Integer.parseInt(startAddress, 16);
         } catch (NumberFormatException ex) {
         }
+        //System.out.println("Start Address = " +startAddress);
+        //System.out.println("LCCTR = " + locctr.toUpperCase());
+        //System.out.println("Program Length = " + programLength);
+
     }
 
     private static void printError(String errormessage) {
-
-        switch (errormessage) {
-            case "duplicate error":
-        }
+        System.out.println(errormessage);
     }
-
 }
